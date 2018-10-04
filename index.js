9572f5798b96f08320f794463a5ddba3ae0484d5
Tomi Laakso <tomi.j.laakso@gmail.com>
2018-10-04 23:27:10 +0300

    final version of exercise 3.5
    Added api for post requests.

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
 
 let persons = [
     {
@@ -23,13 +26,38 @@ let persons = [
         number: '040-123456',
         id: 4
     }
-    
-    
   ]
 
+
+const generateId = () => {
+    let id = randomNumber(); 
+    while( persons.find(person=> person.id === id ) ) {
+        // In case random number is reserved, pick another one
+        id =randomNumber()
+    }
+    return id;
+}
+const randomNumber= ()=> {
+    return Math.floor(Math.random() * 1E6)+1; 
+}
+
 app.get('/api/persons', (request, response) => {
+    console.log('requested all data')
     response.json(persons)
 })
+app.post('/api/persons', (request, response) => {
+    console.log('Data posted to server')
+    const body = request.body
+
+    const person = {
+      name: body.name,
+      number: body.number,
+      id: generateId()
+    }
+    persons = persons.concat(person)
+    response.json(person)
+  })
+  
 app.get('/api/persons/:id', (request, response) => {
     const id = Number(request.params.id)
     console.log(`requested person with id=${id}`)
